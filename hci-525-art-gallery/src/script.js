34dc16688829bfcd75491af971ab61bd266bf5c6
lprinty <128174045+lprinty@users.noreply.github.com>
2023-03-26 17:44:43 -0400

    Timeline Work

---
+++ hci-525-art-gallery/src/script.js
@@ -0,0 +1,22 @@
+var items = document.querySelectorAll('.timeline li');
+
+function elementInView(el){
+    var rect = el.getBoundingClientRect();
+
+    return(
+        rect.top>= 0 &&
+        rect.buttom <= (window.innerHeight || document.documentElement.clientHeight )
+    )
+}
+
+function callBack(){
+    for(var i = 0; i < items.length; i++){
+        if(elementInView(items[i])){
+            items[i].classList.add("in-view")
+        }
+    }
+}
+
+window.onload = callBack;
+window.onresize = callBack;
+window.onscroll = callBack;
